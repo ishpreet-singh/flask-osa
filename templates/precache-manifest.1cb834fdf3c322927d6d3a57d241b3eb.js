41d8e143f8ff74e4885cb5b5daedd32fd8f8c0b4
Ishpreet <ishpreetsingh2808@gmail.com>
2020-06-08 02:44:33 -0400

    Adding New Build

---
+++ templates/precache-manifest.1cb834fdf3c322927d6d3a57d241b3eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37914008432d4d7efb5f5130848c5e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b670d77101d253ac5c0",
+    "url": "/static/css/main.8eb70866.chunk.css"
+  },
+  {
+    "revision": "d1e0898fd534d9d7d644",
+    "url": "/static/js/2.bbd88131.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bbd88131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b670d77101d253ac5c0",
+    "url": "/static/js/main.0182ab04.chunk.js"
+  },
+  {
+    "revision": "0894f2ab3130391ed9e3",
+    "url": "/static/js/runtime-main.58dbad9a.js"
+  },
+  {
+    "revision": "be1ece074388e55791259a7f6e692e1d",
+    "url": "/static/media/oscillator-img.be1ece07.png"
+  }
+]);
--- templates/service-worker.js
+++ templates/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e51a0a8e7f8c735cc4e43ab246b0b9cf.js"
+  "/precache-manifest.1cb834fdf3c322927d6d3a57d241b3eb.js"
 );
 
 self.addEventListener('message', (event) => {
